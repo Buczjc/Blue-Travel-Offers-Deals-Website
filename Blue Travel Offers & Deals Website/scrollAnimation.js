0c2a27fd233b9534de67b11b5327b80cd6a1f112
bucz <119478207+Buczjc@users.noreply.github.com>
2024-08-03 22:09:10 +0800

    Add files via upload

---
+++ Blue Travel Offers & Deals Website/scrollAnimation.js
@@ -0,0 +1,111 @@
+ScrollReveal({
+  reset: true,
+  distance: "30px",
+  duration: 1500,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".logo", {
+  delay: 100,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".first__page__content h1", {
+  delay: 200,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".first__page__content h3", {
+  delay: 300,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".first__page__content button", {
+  delay: 650,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".second__page__content__1", {
+  delay: 200,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".second__page__content__2", {
+  delay: 200,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".third__page__container h1", {
+  delay: 200,
+  origin: "top",
+});
+
+ScrollReveal().reveal(".image-block-1", {
+  delay: 400,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".image-block-2", {
+  delay: 550,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".image-block-3", {
+  delay: 650,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".third__page__container button", {
+  delay: 650,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".fourt-page-content h1", {
+  delay: 200,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".fourt-page-content p", {
+  delay: 400,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".fourt-page-content button", {
+  delay: 650,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".fifth_page_container h1", {
+  delay: 200,
+  origin: "top",
+});
+
+ScrollReveal().reveal(".fifth__page_passenger_block_container", {
+  delay: 400,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".sixth_page_content-1", {
+  delay: 200,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".sixth_page_content-2", {
+  delay: 300,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".main__content__1__f", {
+  delay: 200,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".main__content__2__f", {
+  delay: 300,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".email_footer", {
+  delay: 500,
+  origin: "bottom",
+});
